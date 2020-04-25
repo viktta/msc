0c5f9468dfd2e8b95aabdc105a4629e4aec61dce
viktta <ale07012002@gmail.com>
2020-04-24 22:39:02 -0400

    modified the playlist component

--- msc_frontend/src/Components/PlayLyst/PlayLyst.jsx
+++ msc_frontend/src/Components/PlayLyst/PlayLyst.jsx
@@ -8,3 +8,62 @@ class PlayLyst extends Component {
 }
 
 export default PlayLyst;
+import React, { Component } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+
+class PlayLyst extends Component {
+constructor(props) {
+	super(props);
+	const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+
+	this.state = {
+		loggedIn: token ? true : false,
+		createplaylist: {
+			name: '',
+			public: false,
+		}
+	}
+}
+
+
+getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+createPlaylist() {
+	spotifyApi.createPlaylist().then((response) => {
+		this.setState({
+			createplaylist: {
+				name: response.name,
+				public: response.public
+			}
+		})
+	})
+}
+
+  render() {
+    return (
+<div className="Main-div">
+<form onSubmit={() => this.createPlaylist()}>
+    	<input className="PlayLyst-name" placeholder="Playlist name"></input>
+    	<button type="submit">Create Playlist</button>
+</form>    	
+</div>);
+  }
+}
+
+export default PlayLyst;
