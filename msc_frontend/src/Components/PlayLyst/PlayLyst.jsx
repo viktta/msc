3ccf33348ed5f7d0e1de7b8aa86409a50a73a556
viktta <45240747+viktta@users.noreply.github.com>
2020-04-29 01:23:15 -0700

    commit

--- msc_frontend/src/Components/PlayLyst/PlayLyst.jsx
+++ msc_frontend/src/Components/PlayLyst/PlayLyst.jsx
@@ -1,68 +1,39 @@
 import React, { Component } from "react";
-
-class PlayLyst extends Component {
-  state = {};
-  render() {
-    return <h1>Hello</h1>;
-  }
-}
-
-export default PlayLyst;
-import React, { Component } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 
 const spotifyApi = new SpotifyWebApi();
 
-
 class PlayLyst extends Component {
-constructor(props) {
-	super(props);
-	const params = this.getHashParams();
-    const token = params.access_token;
-    if (token) {
-      spotifyApi.setAccessToken(token);
-    }
-
-	this.state = {
-		loggedIn: token ? true : false,
-		createplaylist: {
-			name: '',
-			public: false,
-		}
-	}
-}
-
-
-getHashParams() {
-    var hashParams = {};
-    var e,
-      r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    while ((e = r.exec(q))) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
+  constructor(props) {
+    super(props);
+    this.state = {
+      createplaylist: {
+        name: "",
+        public: false,
+      },
+    };
   }
 
-createPlaylist() {
-	spotifyApi.createPlaylist().then((response) => {
-		this.setState({
-			createplaylist: {
-				name: response.name,
-				public: response.public
-			}
-		})
-	})
-}
+  createPlaylist() {
+    spotifyApi.createPlaylist().then((response) => {
+      this.setState({
+        createplaylist: {
+          name: response.name,
+          public: response.public,
+        },
+      });
+    });
+  }
 
   render() {
     return (
-<div className="Main-div">
-<form onSubmit={() => this.createPlaylist()}>
-    	<input className="PlayLyst-name" placeholder="Playlist name"></input>
-    	<button type="submit">Create Playlist</button>
-</form>    	
-</div>);
+      <div className="Main-div">
+        <form onSubmit={() => this.createPlaylist()}>
+          <input type="text" placeholder="Create Playlist"></input>
+          <button type="submit">Create Playlist</button>
+        </form>
+      </div>
+    );
   }
 }
 
